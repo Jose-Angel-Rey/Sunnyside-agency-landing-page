355b55fdb1ad5a89a8204063f5b8f5be04b6ff6c
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-06-16 07:24:36 -0400

    Add ScrollReveal animations

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -3,9 +3,61 @@
 // *******************************
 
 // -----  Main header -----
+
 ScrollReveal().reveal(".main-header__navbar", {
   duration: 2000,
   delay: 500,
+  origin: "right",
+  distance: "3rem",
+});
+
+ScrollReveal().reveal(".main-header__title", {
+  duration: 2500,
+  delay: 750,
   origin: "top",
   distance: "2rem",
 });
+
+ScrollReveal().reveal(".transform-section__picture", {
+  duration: 2000,
+  delay: 750,
+  origin: "right",
+  distance: "2rem",
+});
+ScrollReveal().reveal(".transform-section__article", {
+  duration: 2000,
+  delay: 750,
+  origin: "left",
+  distance: "2rem",
+});
+ScrollReveal().reveal(".stand-out-section__picture", {
+  duration: 2000,
+  delay: 750,
+  origin: "left",
+  distance: "2rem",
+});
+ScrollReveal().reveal(".stand-out-section__article", {
+  duration: 2000,
+  delay: 750,
+  origin: "right",
+  distance: "2rem",
+});
+ScrollReveal().reveal(".graphic-design-section", {
+  duration: 2000,
+  delay: 750,
+  origin: "left",
+  distance: "2rem",
+});
+ScrollReveal().reveal(".photography-section", {
+  duration: 2000,
+  delay: 750,
+  origin: "right",
+  distance: "2rem",
+});
+
+// ScrollReveal().reveal(".", {
+//   duration: 2000,
+//   delay: 500,
+//   origin: "left",
+//   distance: "2rem",
+// });
