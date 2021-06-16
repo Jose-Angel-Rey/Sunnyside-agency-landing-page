637c03c8296e345daa1a7a3e4c1a77e42a8eb19b
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-06-16 07:07:17 -0400

    Update

---
+++ js/scroll-reveal.js
@@ -0,0 +1,11 @@
+// *******************************
+//      ScrollReveal animations
+// *******************************
+
+// -----  Main header -----
+ScrollReveal().reveal(".main-header__navbar", {
+  duration: 2000,
+  delay: 500,
+  origin: "top",
+  distance: "2rem",
+});
