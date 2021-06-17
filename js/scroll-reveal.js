8524b78277a20710c38d62c95eb9fc4283b93e60
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-06-17 07:37:54 -0400

    Update scroll-reveal.js

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -3,100 +3,100 @@
 // *******************************
 
 // -----  Main content -----
-ScrollReveal().reveal(".transform-section__picture", {
+ScrollReveal().reveal(".transform-section__image", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "right",
   distance: "2rem",
 });
 ScrollReveal().reveal(".transform-section__article", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "left",
   distance: "2rem",
 });
-ScrollReveal().reveal(".stand-out-section__picture", {
+ScrollReveal().reveal(".stand-out-section__image", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "left",
   distance: "2rem",
 });
 ScrollReveal().reveal(".stand-out-section__article", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "right",
   distance: "2rem",
 });
 ScrollReveal().reveal(".graphic-design-section", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "left",
   distance: "2rem",
 });
 ScrollReveal().reveal(".photography-section", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "right",
   distance: "2rem",
 });
 ScrollReveal().reveal(".testimonials__title", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "bottom",
   distance: "2rem",
 });
 
 ScrollReveal().reveal(".testimonial:first-of-type", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "left",
   distance: "2rem",
 });
 
 ScrollReveal().reveal(".testimonial:nth-child(2)", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "bottom",
   distance: "2rem",
 });
 
 ScrollReveal().reveal(".testimonial:last-of-type", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "right",
   distance: "2rem",
 });
 // -----  Gallery -----
 ScrollReveal().reveal(".gallery picture:nth-child(odd)", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "bottom",
   distance: "3rem",
 });
 
 ScrollReveal().reveal(".gallery picture:nth-child(even)", {
   duration: 2000,
-  delay: 1000,
+  delay: 700,
   origin: "top",
   distance: "3rem",
 });
 // -----  Main footer -----
 ScrollReveal().reveal(".main-footer__navbar", {
   duration: 2000,
-  delay: 750,
+  delay: 500,
   origin: "right",
   distance: "2rem",
 });
 ScrollReveal().reveal(".social-media-icons", {
   duration: 2000,
-  delay: 1250,
+  delay: 700,
   origin: "bottom",
   distance: "2rem",
 });
 // -----  Attribution -----
 ScrollReveal().reveal(".attribution p", {
   duration: 2000,
-  delay: 1750,
+  delay: 800,
   origin: "top",
   distance: "3rem",
 });
